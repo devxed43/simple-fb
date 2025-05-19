08c7763a67ef23fdaacaa98d67080c8ee5b28c70
Edward <edwlope@Edwards-MacBook-Pro.local>
2025-05-19 16:02:26 -0700

    update validation/auth logic --> sign in fn()

--- app.js
+++ app.js
@@ -36,16 +36,38 @@ var newsfeed = [
   },
 ];
 
+// data we get from our user
 var usernamePrompt = prompt("enter username: ");
 var passwordPrompt = prompt("enter password: ");
 
+// checks if user entry is valid
+// loops over database, if matches, 
+// return boolean of true or false
+function validator(user, pass) {
+  // as we loop over database, do a check
+  // true segment
+  for (var i = 0; i < database.length; i++) {
+    // each iteration of database
+    // if username in database matches data our user puts in
+    if (
+      database[i].username === user &&
+      database[i].password === pass
+    ) {
+      return true;
+    }
+  }
+  // false segment
+  return false;
+}
+
+// our sign in accepts arguments that we can dynamically input
+// user, pass is the data our user enters
+// validator accepts the prompts, 
 function signIn(user, pass) {
-  if (user === database[0].username && 
-      pass === database[0].password) {
-    // console.log(`${user}'s timeline:`, newsfeed[0].timeline);
-    alert(`welcome ${user}, you are now logged in`)
+  if (validator(user, pass)) {
+    console.log(newsfeed);
   } else {
-    alert("wrong username or password");
+    alert("invalid. try again");
   }
 }
 
@@ -58,3 +80,13 @@ signIn(usernamePrompt, passwordPrompt);
 // these arguments get checked against the database list item password
 // if match, we display via console.log our newsfeed list, 0 index item(first)
 // via dot notation, we get the specific piece of data timeline
+
+
+// FLOW OF DATA INPUT
+
+// user prompts --> validator 
+// validator, does check, returns boolean -> signIn
+// signIn accepts the data from the validator, if true/match --> show newsfeed
+
+// IN CONCLUSION:
+// prompts -> validate authentication -> if authentication correct -> show sensitive data
