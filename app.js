dbb98d4f70210882f0ca385daf82d875e0d769f6
Edward <edwlope@Edwards-MacBook-Pro.local>
2025-05-19 14:40:44 -0700

    first draft

--- app.js
+++ app.js
@@ -23,18 +23,36 @@ var database = [
 // return timeline
 var newsfeed = [
   {
-    username: "ashley",
+    username: "edward",
     timeline: "JS is cool",
   },
   {
-    username: "becky",
+    username: "steven",
     timeline: "Help! i'm confused",
   },
   {
-    username: "cindy",
+    username: "oscar",
     timeline: "I have a lot of ideas on what to build",
   },
 ];
 
-user_username = prompt("enter username: ");
-user_password = prompt("enter password: ");
+var usernamePrompt = prompt("enter username: ");
+var passwordPrompt = prompt("enter password: ");
+
+function signIn(user, pass) {
+  if (user === database[0].username && pass === database[0].password) {
+    console.log(`${user}'s timeline:`, newsfeed[0].timeline);
+  } else {
+    alert("wrong username or password");
+  }
+}
+
+signIn(usernamePrompt, passwordPrompt);
+// ==================== REFLECTION ====================
+// we call the function with dynamic data that our user types in
+// the unPrompt, pwPrompt sync to the argument keywords
+// user, pass,
+// these arguments get checked against the database list item username
+// these arguments get checked against the database list item password
+// if match, we display via console.log our newsfeed list, 0 index item(first)
+// via dot notation, we get the specific piece of data timeline
